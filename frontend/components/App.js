f6e5915a618d6ddb547d2312ed50bfe51965ffac
Landon Phillips <landonphillips@Landons-MBP.attlocal.net>
2022-04-12 09:44:04 -0500

    completed state and completed flag

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,37 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+       
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state;
+    
     return (
       <div>
-        <h1>Todos </h1>
+        <h1>Todos:</h1>
         <ul>
-          <li>Walk the dog</li>
-          <li>Take out trash</li>
-          <li>clean bedroom</li>
+          {
+            todos.map(todo => {
+              return (<li>{todo.name} { todo.completed ? <span> - Completed</span> : <span></span>}</li>)
+            })
+          }
+          
         </ul>
         <form>
           <input />
